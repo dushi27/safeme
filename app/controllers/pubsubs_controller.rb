be454f49548918b53aef5eee8363dbbf6a115637
dushi27 <dushi27@gmail.com>
2015-06-05 00:08:28 +0000

    update the pubsub url to recieve requests

--- app/controllers/pubsubs_controller.rb
+++ app/controllers/pubsubs_controller.rb
@@ -1,32 +1,29 @@
 class PubsubsController < ApplicationController
-  before_action :set_pubsub, only: [:show, :edit, :update, :destroy]
+  #before_action :set_pubsub, only: [:show, :edit, :update, :destroy]
 
-  # GET /pubsubs
-  # GET /pubsubs.json
+    
   def index
     @pubsubs = Pubsub.all
   end
 
-  # GET /pubsubs/1
-  # GET /pubsubs/1.json
+    
   def show
   end
 
-  # GET /pubsubs/new
+    
   def new
     @pubsub = Pubsub.new
   end
 
-  # GET /pubsubs/1/edit
+    
   def edit
   end
 
-  # POST /pubsubs
-  # POST /pubsubs.json
   def create
-    @pubsub = Pubsub.new(pubsub_params)
-
-    respond_to do |format|
+      
+    @pubsub = Pubsub.new(:user_xid => params[:events][:user_xid], :type => params[:events][:type])
+=begin
+    respond_to do |format|    
       if @pubsub.save
         format.html { redirect_to @pubsub, notice: 'Pubsub was successfully created.' }
         format.json { render :show, status: :created, location: @pubsub }
@@ -35,10 +32,20 @@ class PubsubsController < ApplicationController
         format.json { render json: @pubsub.errors, status: :unprocessable_entity }
       end
     end
+=end
+      if @pubsub.save
+            render :json => {
+      :user => 200
+    }
+      else
+                      render :json => {
+      :user => 200
+    }
+      end
+          
   end
 
-  # PATCH/PUT /pubsubs/1
-  # PATCH/PUT /pubsubs/1.json
+    
   def update
     respond_to do |format|
       if @pubsub.update(pubsub_params)
@@ -51,8 +58,7 @@ class PubsubsController < ApplicationController
     end
   end
 
-  # DELETE /pubsubs/1
-  # DELETE /pubsubs/1.json
+    
   def destroy
     @pubsub.destroy
     respond_to do |format|
@@ -62,12 +68,11 @@ class PubsubsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pubsub
       @pubsub = Pubsub.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def pubsub_params
       params.require(:pubsub).permit(:user_xid, :type, :action, :timestamp)
     end
