e0cb3efd8f633681ea10de08e7692a0334c69673
dushi27 <dushi27@gmail.com>
2015-06-10 04:56:47 +0000

    update controller to get jawbone data

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,6 +1,9 @@
 class BandController < ApplicationController
   skip_before_action :verify_authenticity_token
     def jawbone
+        @data = params["events"].first["user_xid"]
+        @event = Jawbone.new(:user_xid => @data)
+        @event.save
        render :json => {:success => 200} 
     end
 end
