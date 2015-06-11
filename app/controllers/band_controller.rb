191de068a0b75d2e8a3b8fb975bce2440e69b54a
dushi27 <dushi27@gmail.com>
2015-06-11 02:06:29 +0000

    update for unknown attribute issue

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,13 +1,13 @@
 class BandController < ApplicationController
   skip_before_action :verify_authenticity_token
     def jawbone
-        if params[:events]
+        unless params[:events].nil?
             @data = params[:events] 
-            @event = Jawbone.new(:data => @data) 
+            @event = Jawbone.new(:data => params[:events] ) 
             if @event.save
                render :json => {:success => 200} 
             else
-                render :json => {:error => 400} 
+               render :json => {:error => 400} 
             end
         else
             render :json => {:error => 400} 
