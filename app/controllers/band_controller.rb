5c76832b47d5fee14ffb1686d1dc6070ac92fd94
dushi27 <dushi27@gmail.com>
2015-06-11 01:46:59 +0000

    add jawbone webhook params

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,12 +1,16 @@
 class BandController < ApplicationController
   skip_before_action :verify_authenticity_token
     def jawbone
-        @data = params["events"]
-        @event = Jawbone.new(:data => @data)
-        if @event.save
-       render :json => {:success => 200} 
+        if params["events"]
+            @data = params["events"] 
+            @event = Jawbone.new(:data => @data) 
+            if @event.save
+               render :json => {:success => 200} 
+            else
+                render :json => {:error => 400} 
+            end
         else
-             render :json => {:success => 200} 
+            render :json => {:error => 400} 
         end
     end
 
