ee83b8238089993141b2963d99868056c1d58323
dushi27 <dushi27@gmail.com>
2015-06-11 04:21:47 +0000

    bug fix on should_alert? helper

--- app/helpers/jawbones_helper.rb
+++ app/helpers/jawbones_helper.rb
@@ -5,7 +5,7 @@ module JawbonesHelper
         @events = Jawbone.where(:user_xid => event.user_xid).limit(3)
         alert = false and return if @events.count < 3
         if (@events.last.created_at - @events[1].created_at).in_milliseconds < 60000
-            alert = true and return if (@events[1] - @events.first).in_milliseconds < 60000            
+            alert = true and return if (@events[1].created_at - @events.first.created_at).in_milliseconds < 60000            
         end
     end
     
