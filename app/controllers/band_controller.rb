557132a15b42d53d6635ac37ea09f1a3ba4d568e
dushi27 <dushi27@gmail.com>
2015-06-11 04:11:40 +0000

    updates to twillio methods

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,6 +1,7 @@
 class BandController < ApplicationController
   include JawbonesHelper
   skip_before_action :verify_authenticity_token
+    before_action :init_twillio
     def jawbone
         unless params[:events].nil?
             count = Jawbone.all.count
@@ -8,12 +9,13 @@ class BandController < ApplicationController
           params[:events].each do |event|
             @event = Jawbone.new(:user_xid => event[:user_xid], :type => event[:type], :action => event[:action], :data => params[:events].to_s )
             @event.save
-            if should_alert?(@event)  
-                @twilio_client.account.sms.messages.create(
-                  :from => ENV['TWILLIO_NUMBER'],
-                  :to => ENV['TWILLIO_NUMBER'],
-                    :body => "Safe.me recognized an alert"
-                )
+                if should_alert?(@event)  
+                    @twilio_client.account.sms.messages.create(
+                        :from => '+15005550006',
+                        :to => twilio_phone_number,
+                        :body => "Safe.me recognized an alert"
+                        )
+                end
             end
           end
           
@@ -28,5 +30,18 @@ class BandController < ApplicationController
             render :json => {:error => 401} 
         end
     end
+    
+    
+    private
+    
+    
+    def init_twillio
+      twilio_sid = ENV['TWILLIO_SID']
+      twilio_token = ENV['TWILLIO_TOKEN']
+      twilio_phone_number = ENV['TWILLIO_NUMBER']
+
+      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    end
+
 
 end
