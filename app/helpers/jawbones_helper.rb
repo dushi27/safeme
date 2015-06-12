f947621b350ff497876972e76da69e594101fc56
dushi27 <dushi27@gmail.com>
2015-06-12 04:43:37 +0000

    update the should alert method

--- app/helpers/jawbones_helper.rb
+++ app/helpers/jawbones_helper.rb
@@ -6,7 +6,9 @@ module JawbonesHelper
         alert = false and return if @events.count < 3 or @events[0].action != 'enter_sleep_mode'
         
         if (@events.last.created_at - @events[1].created_at).in_milliseconds < 60000
-            alert = true and return if (@events[1].created_at - @events.first.created_at).in_milliseconds < 60000            
+            if (@events[1].created_at - @events.first.created_at).in_milliseconds < 60000       
+                alert = true 
+            end
         end
     end
     
