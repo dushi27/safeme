81720632bed214c7b7d5174e011d640d36816f3b
dushi27 <dushi27@gmail.com>
2015-06-14 17:46:48 +0000

    bug fix on jawbone create

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -7,17 +7,12 @@ class BandController < ApplicationController
         count = Jawbone.all.count
             
         params[:events].each do |event|
-            count - 1 and next if (Time.now.to_i - event[:timestamp] ) < 60000
+            count - 1 and next if (Time.now.to_i - event[:timestamp] ) > 60000
             @event = Jawbone.new(:user_xid => event[:user_xid], :action => event[:action], :data => params[:events].to_s )
             @event.save
                 if should_alert?(@event) 
                   puts "ALERT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
                   
-                  twilio_sid = ENV['TWILLIO_SID']
-                  twilio_token = ENV['TWILLIO_TOKEN']
-                  twilio_phone_number = ENV['TWILLIO_NUMBER']
-                    
-                  @client = Twilio::REST::Client.new twilio_sid, twilio_token
                   to_number = User.where(:xid => @event.user_xid).first.my_num
                   message = @client.account.messages.create(
                     :to => to_number,
--- config/initializers/twillio_init.rb
+++ config/initializers/twillio_init.rb
@@ -1,5 +1,5 @@
 twilio_sid = ENV['TWILLIO_SID']
 twilio_token = ENV['TWILLIO_TOKEN']
 twilio_phone_number = ENV['TWILLIO_NUMBER']
-
-@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+                    
+@client = Twilio::REST::Client.new twilio_sid, twilio_token
