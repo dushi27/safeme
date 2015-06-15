afc3c7cd0b2842467d349897f8b9ca68ad4e0f41
dushi27 <dushi27@gmail.com>
2015-06-15 00:39:21 +0000

    update should_alert? helper logic

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -8,10 +8,10 @@ class BandController < ApplicationController
       render :json => {:error => 400} and return if params[:events].nil?
         
       params[:events].each do |event|
-          next if (Time.now.to_i - event[:timestamp] ) > 60 or event[:action] == 'updation'
-          @event = Jawbone.create(:user_xid => event[:user_xid], :event_xid => event[:timestamp], :action => event[:action], :data => params[:events].to_s)
-            if @event
-              if should_alert?(@event) 
+        next if (Time.now.to_i - event[:timestamp] ) > 60 or event[:action] != 'enter_sleep_mode' or event[:action] != 'exit_sleep_mode' 
+        @event = Jawbone.create(:user_xid => event[:user_xid], :event_xid => event[:timestamp], :action => event[:action], :data => params[:events].to_s)
+        if @event
+          if should_alert?(@event) 
                 puts "ALERT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"                  
                 twilio_phone_number = ENV['TWILLIO_NUMBER']
                 to_number = User.where(:xid => @event.user_xid).first.my_num
--- app/helpers/jawbones_helper.rb
+++ app/helpers/jawbones_helper.rb
@@ -2,14 +2,12 @@ module JawbonesHelper
     
     
     def should_alert?(event)
-        @events = Jawbone.where(:user_xid => event.user_xid).limit(3)
-        alert = false and return if @events.count < 3 or @events[0].action != 'enter_sleep_mode'
+     @events = Jawbone.where(:user_xid => event.user_xid).order(:created_at).limit(3)
+        
+     alert = false and return if @events.count < 3 or (@events.last.created_at - @events.first.created_at).in_milliseconds > 60000
+     alert = false and return unless @events[0].action == 'enter_sleep_mode' and @events[1].action == 'exit_sleep_mode' and @events[2].action == 'enter_sleep_mode'
        
-        if (@events.last.created_at - @events[1].created_at).in_milliseconds < 60000
-            if (@events[1].created_at - @events.first.created_at).in_milliseconds < 60000       
-                alert = true 
-            end
-        end
+      alert = true 
     end
     
     
