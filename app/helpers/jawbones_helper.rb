a9c9295e610802cde2c80fb3e9ecdd4944766897
dushi27 <dushi27@gmail.com>
2015-06-22 02:16:15 +0000

    update should alertt

--- app/helpers/jawbones_helper.rb
+++ app/helpers/jawbones_helper.rb
@@ -1,13 +1,11 @@
-module JawbonesHelper    
-    
+module JawbonesHelper       
     
     def should_alert?(event)
-     @events = Jawbone.where(:user_xid => event.user_xid).order(:created_at).limit(3)
-        
-     alert = false and return if @events.count < 3 or (@events.last.created_at - @events.first.created_at).in_milliseconds > 60000
-     alert = false and return unless @events[0].action == 'enter_sleep_mode' and @events[1].action == 'exit_sleep_mode' and @events[2].action == 'enter_sleep_mode'
-       
-      alert = true 
+     @events = Jawbone.where(:user_xid => event.user_xid).order(:created_at).limit(3)        
+     return if @events.count < 3 
+     return if (@events.last.created_at - @events.first.created_at).in_milliseconds > 60000
+     return unless @events[0].action == 'enter_sleep_mode' and @events[1].action == 'exit_sleep_mode' and @events[2].action == 'enter_sleep_mode'
+     alert = true 
     end
     
     
