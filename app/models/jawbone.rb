296f3c8a619ab78dea63cd1ca2a5eac14031ed69
dushi27 <dushi27@gmail.com>
2015-06-23 00:56:33 +0000

    update alert

--- app/models/jawbone.rb
+++ app/models/jawbone.rb
@@ -2,9 +2,9 @@ class Jawbone < ActiveRecord::Base
     after_commit :should_alert, on: :create
     #validates :event_xid, uniqueness: true
    def should_alert
-     events = Jawbone.where(:user_xid => self.user_xid).order(:created_at).limit(3)        
+     events = Jawbone.where(:user_xid => self.user_xid).order('created_At DESC').limit(3)        
      return if events.count < 3 
-     return if (events.last.created_at - events.first.created_at).in_milliseconds > 60000
+     return if (events.first.created_at - events.last.created_at).in_milliseconds > 60000
      return unless events[0].action == 'enter_sleep_mode' and events[1].action == 'exit_sleep_mode' and events[2].action == 'enter_sleep_mode'
      
      twilio_sid = ENV['TWILLIO_SID']
