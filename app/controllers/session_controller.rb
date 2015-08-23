7262df2c9ee30494d493b4f21495f94b87b9cc43
Dushyanthi Pieris <dushi@ovlu.co>
2015-08-23 19:31:40 +0000

    bug fix in login/out

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,16 +1,9 @@
 class SessionController < ApplicationController
+ before_action :validations, only: [:create]
     def show
     end
     
     def create 
-     @user = User.where(:email => params[:email]).first
-
-     #validations
-     redirect_to root_path, :flash => {notice: 'Invalid user'} and return if @user.nil?
-     redirect_to edit_user_path(@user.id), :flash => {notice: 'Your are already logged in'} and return if !session[:hex].nil?      
-     redirect_to root_path, :flash => {notice: 'Incorrect password'} and return if @user.password !=  Digest::MD5.hexdigest(params[:password])
-
-     #login
      @hex = SecureRandom.hex
      session[:hex] = @hex
      session[:uid] = @user.id
@@ -24,9 +17,19 @@ class SessionController < ApplicationController
      unless session[:uid].nil? and session[:hex].nil? and @user.session.nil?  
        @user = User.find(session[:uid])
        session[:hex] = nil
+       session[:uid] = nil
        @user.session = nil
        @user.save            
      end
        redirect_to root_path, notice: 'See you later'
+    end
+    
+    private
+    
+    def validations
+     @user = User.where(:email => params[:email]).first
+     redirect_to root_path, :flash => {notice: 'Invalid user'} and return if @user.nil?
+     redirect_to edit_user_path(@user.id), :flash => {notice: 'Your are already logged in'} and return if !session[:hex].nil?      
+     redirect_to root_path, :flash => {notice: 'Incorrect password'} and return if @user.password !=  Digest::MD5.hexdigest(params[:password])
     end
 end
