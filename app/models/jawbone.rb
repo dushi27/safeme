7d98dcc006da7c9d5cea3d678e5d8b76aa015fd3
Dushyanthi Pieris <dushi@ovlu.co>
2015-08-24 02:55:53 +0000

    fix for no jawbone pubsub contet error

--- app/models/jawbone.rb
+++ app/models/jawbone.rb
@@ -5,9 +5,9 @@ class Jawbone < ActiveRecord::Base
     
   def should_alert?
     events = Jawbone.find_by_sql("SELECT * from jawbones where user_xid='#{self.user_xid}' AND responded <> true ORDER BY timestamp DESC LIMIT 3")  
-   #and action =     'enter_sleep_mode' OR action = 'exit_sleep_mode' 
+    #and action = 'enter_sleep_mode' OR action = 'exit_sleep_mode' 
     return if events.count < 3 or (events.first.timestamp.to_i - events.last.timestamp.to_i) > 60
-    events.each {|e| return unless e.action == 'enter_sleep_mode' or e.action 'exit_sleep_mode'}
+    #events.each {|e| return unless e.action == 'enter_sleep_mode' or e.action 'exit_sleep_mode'}
     true   
   end    
     
